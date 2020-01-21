252adc352fb789e80b257618d60aedc26028d21b
alng209 <aln.g99@mail.ru>
2020-01-21 17:33:59 +0300

    add kurs

---
+++ glushenkova/kursovayea/tictactoe/src/view/Board.java
@@ -0,0 +1,18 @@
+package view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Board extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
